fd78d4b752e4bfb7f96c73c4c1dee2c5d26a2db6
Mustafa Çağlar KARA <mustafacaglarkara@gmail.com>
2024-02-19 19:04:28 +0300

    Create yorumSatiri.pas
    Yorum ekleme

---
+++ yorumSatiri.pas
@@ -0,0 +1,20 @@
+program mckMain;
+
+uses
+crt;
+(*
+Çoklu Yorum Satısı
+*)
+
+// Yorum Satısı
+
+{ Bu da farklı yorum satısı }
+
+var 
+karakter : Char;
+
+begin
+  ReadLn(karakter); 
+  WriteLn(karakter);
+  readln;
+end.
